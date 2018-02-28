7d4cb164e119b373a6ed853ca303bc20e72c442a
PrettierCI <commits@prettierci.com>
2018-02-28 10:58:03 -0800

    Sync with Prettier

--- src/js/control-bar/mute-toggle.js
+++ src/js/control-bar/mute-toggle.js
@@ -6,39 +6,49 @@
  */
 vjs.MuteToggle = vjs.Button.extend({
   /** @constructor */
-  init: function(player, options){
+  init: function(player, options) {
     vjs.Button.call(this, player, options);
 
-    player.on('volumechange', vjs.bind(this, this.update));
+    player.on("volumechange", vjs.bind(this, this.update));
 
     // hide mute toggle if the current tech doesn't support volume control
-    if (player.tech && player.tech.features && player.tech.features.volumeControl === false) {
-      this.addClass('vjs-hidden');
+    if (
+      player.tech &&
+      player.tech.features &&
+      player.tech.features.volumeControl === false
+    ) {
+      this.addClass("vjs-hidden");
     }
-    player.on('loadstart', vjs.bind(this, function(){
-      if (player.tech.features && player.tech.features.volumeControl === false) {
-        this.addClass('vjs-hidden');
-      } else {
-        this.removeClass('vjs-hidden');
-      }
-    }));
+    player.on(
+      "loadstart",
+      vjs.bind(this, function() {
+        if (
+          player.tech.features &&
+          player.tech.features.volumeControl === false
+        ) {
+          this.addClass("vjs-hidden");
+        } else {
+          this.removeClass("vjs-hidden");
+        }
+      })
+    );
   }
 });
 
-vjs.MuteToggle.prototype.createEl = function(){
-  return vjs.Button.prototype.createEl.call(this, 'div', {
-    className: 'vjs-mute-control vjs-control',
+vjs.MuteToggle.prototype.createEl = function() {
+  return vjs.Button.prototype.createEl.call(this, "div", {
+    className: "vjs-mute-control vjs-control",
     innerHTML: '<div><span class="vjs-control-text">Mute</span></div>'
   });
 };
 
-vjs.MuteToggle.prototype.onClick = function(){
-  this.player_.muted( this.player_.muted() ? false : true );
+vjs.MuteToggle.prototype.onClick = function() {
+  this.player_.muted(this.player_.muted() ? false : true);
 };
 
-vjs.MuteToggle.prototype.update = function(){
+vjs.MuteToggle.prototype.update = function() {
   var vol = this.player_.volume(),
-      level = 3;
+    level = 3;
 
   if (vol === 0 || this.player_.muted()) {
     level = 0;
@@ -51,19 +61,19 @@ vjs.MuteToggle.prototype.update = function(){
   // Don't rewrite the button text if the actual text doesn't change.
   // This causes unnecessary and confusing information for screen reader users.
   // This check is needed because this function gets called every time the volume level is changed.
-  if(this.player_.muted()){
-      if(this.el_.children[0].children[0].innerHTML!='Unmute'){
-          this.el_.children[0].children[0].innerHTML = 'Unmute'; // change the button text to "Unmute"
-      }
+  if (this.player_.muted()) {
+    if (this.el_.children[0].children[0].innerHTML != "Unmute") {
+      this.el_.children[0].children[0].innerHTML = "Unmute"; // change the button text to "Unmute"
+    }
   } else {
-      if(this.el_.children[0].children[0].innerHTML!='Mute'){
-          this.el_.children[0].children[0].innerHTML = 'Mute'; // change the button text to "Mute"
-      }
+    if (this.el_.children[0].children[0].innerHTML != "Mute") {
+      this.el_.children[0].children[0].innerHTML = "Mute"; // change the button text to "Mute"
+    }
   }
 
   /* TODO improve muted icon classes */
   for (var i = 0; i < 4; i++) {
-    vjs.removeClass(this.el_, 'vjs-vol-'+i);
+    vjs.removeClass(this.el_, "vjs-vol-" + i);
   }
-  vjs.addClass(this.el_, 'vjs-vol-'+level);
+  vjs.addClass(this.el_, "vjs-vol-" + level);
 };
